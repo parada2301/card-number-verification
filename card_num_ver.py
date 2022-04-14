db897fda1d4470473212087f9217e409392b91a2
Andres Parada <andresparada@Kaylees-Air.lan>
2022-04-13 20:21:15 -0600

    updated

--- card_num_ver.py
+++ card_num_ver.py
@@ -1,16 +1,38 @@
 import getpass as gp
 
-from numpy import True_
+card_num = ''
 class Card:
-    def __init__(self):
+    
+    #keeps the check_len loop running until is changed to False
+    check_len = True  
+    
+    #checks the string lenght of the card is correct either 16 or
+    #15 digits for AMEX cards     
+    while check_len:
+        
+        #requests card number using the getpass library
         card_num = str(gp.getpass('Input credit card number to validate:    '))
-        check_len = True
-        while check_len:
-            if len(card_num) == 16 or len(card_num) == 15:
-                self.card_num = card_num
-                check_len = False
-            else:
-                print('\nCard number needs to by 15 or 16 digits! Try again.\n')
+
+        #checks if input is of expected length and that it is a number 
+        if (len(card_num) != 16 and len(card_num) != 15) or card_num.isdigit != True:
+            
+            #if card number is not 15 or 16 digits long, or input is not a digit
+            #this message is shown and the loop is restarted to ask for number again.
+            print('\nCard number needs to by 15 or 16 digits! Try again.\n')               
+        else:
+
+            #if input is valid the While loop is stopped and variable card_num stays
+            #as user input to be added as property of the class.
+            check_len = False
+    
+    #checks if credit card number follows luhn algorithm, which is used to verify a card
+    #number is a valid number
+    def validate(card_num):
+        pass
+    def __init__(self):
+        self.card_num = card_num
+        
+                
         
 
 first_card = Card()
